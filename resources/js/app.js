1e938c7bec1a2a0129018bbbe9f5e44e5f017583
Romitou <romitou@protonmail.com>
2020-08-26 10:43:59 +0200

    :bento: Updating assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,2 @@
 require('./bootstrap');
 require('./sb-admin-2.min')
-require('@fortawesome/fontawesome-free/js/all')
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
+    .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
