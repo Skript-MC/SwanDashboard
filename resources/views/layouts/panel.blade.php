ffa36f9fb0d1f2af76cb2c244bb8a2a2edf98d10
Romitou <romitou@protonmail.com>
2020-09-26 19:48:34 +0200

    :lipstick: Removing search bar

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Swan Dashboard - @yield('title')</title>
     <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+    <script src="{{ asset(mix('js/sb-admin.js')) }}"></script>
+    <script src="{{ asset(mix('js/bootstrap.js')) }}"></script>
 </head>
 <body id="page-top">
 <div id="wrapper">
@@ -51,7 +53,7 @@
         <li class="nav-item {{ request()->is('sentry') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('sentry') }}">
                 <i class="fas fa-exclamation-circle"></i>
-                <span>Exceptions Sentry</span></a>
+                <span>Rapports Sentry</span></a>
         </li>
         <hr class="sidebar-divider d-none d-md-block">
         <div class="text-center d-none d-md-inline">
@@ -61,37 +63,7 @@
     <div id="content-wrapper" class="d-flex flex-column">
         <div id="content">
             <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button">
-                                <i class="fas fa-search fa-sm"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown no-arrow d-sm-none">
-                        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-search fa-fw"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                            <form class="form-inline mr-auto w-100 navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fas fa-search fa-sm"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li>
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
@@ -144,6 +116,5 @@
         </div>
     </div>
 </div>
-<script src="{{ asset(mix('js/app.js')) }}"></script>
 </body>
 </html>
