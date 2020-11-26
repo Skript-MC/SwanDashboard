237d9cbab429c61f6741c4c9b633dfc91d57ef9d
Romitou <romitou@protonmail.com>
2020-11-26 15:20:02 +0100

    :bento: Adding assets and Webpack

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,8 @@
-import './styles/app.css';
-console.log('test');
+// Import SB Admin styles and JS
+import './scss/sb-admin-2.scss';
+import './js/sb-admin';
+
+// Import FontAwesome icons
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all';
+
