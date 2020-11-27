4754c8f44b26b5d1c9f6ecda551fffcff68bd7c2
Romitou <romitou@protonmail.com>
2020-11-27 19:54:24 +0100

    :sparkles: Improving DiscordTimestampExtension for 24 hours date format.

--- src/Twig/DiscordTimestampExtension.php
+++ src/Twig/DiscordTimestampExtension.php
@@ -14,9 +14,9 @@ class DiscordTimestampExtension extends AbstractExtension
         ];
     }
 
-    public function formatTimestamp($snowflake)
+    public function formatTimestamp(string $snowflake)
     {
         $time = (string)($snowflake >> 22);
-        return date('d/m/yy à h:m A', (((int)substr($time, 0, -3)) + 1420070400) . '.' . substr($time, -3));
+        return date('d/m/yy à H:i', (((int) substr($time, 0, -3)) + 1420070400) . '.' . substr($time, -3));
     }
 }
