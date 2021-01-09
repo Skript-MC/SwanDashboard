528c78ee94675507887a9df4d31a187dd927b506
Romitou <romitou@protonmail.com>
2021-01-09 23:21:11 +0100

    :sparkles: Add discord member count to dashboard

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Discord\SwanClient;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,13 +13,18 @@ class MainController extends AbstractController
 {
     /**
      * @Route("", name="home")
+     * @param SwanClient $swanClient
      * @return Response
      */
-    public function home(): Response
+    public function home(SwanClient $swanClient): Response
     {
         if (!$this->isGranted('ROLE_USER'))
             return $this->render('welcome.html.twig');
-        return $this->render('dashboard.html.twig');
+        $guild = $swanClient->getGuild();
+        return $this->render('dashboard.html.twig', [
+            'discordMembers' => $guild->getMemberCount(),
+            'discordOnlineMembers' => $guild->getPresenceCount()
+        ]);
     }
 
     /**
