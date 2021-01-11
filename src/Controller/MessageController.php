efa102038f05a9a0547b4fada9511c97dd29f118
Romitou <romitou@protonmail.com>
2021-01-11 15:43:24 +0100

    :sparkles: Introducing admin suggestions approvals

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -26,18 +26,21 @@ class MessageController extends AbstractController
 {
     /**
      * @Route("", name="messages")
+     * @param Request $request
      * @param DocumentManager $dm
      * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function home(DocumentManager $dm, PaginatorInterface $paginator): Response
+    public function home(Request $request, DocumentManager $dm, PaginatorInterface $paginator): Response
     {
         /** @var User $user */
         $user = $this->getUser();
         $requests = $paginator->paginate(
             $dm->createQueryBuilder(MessageEditRequest::class)
                 ->field('user.id')->equals($user->getId())
-                ->getQuery()
+                ->sort( '_id', 'DESC')
+                ->getQuery(),
+            $request->query->getInt('page', 1)
         );
         return $this->render('messages/home.html.twig', [
             'requests' => $requests
@@ -46,43 +49,55 @@ class MessageController extends AbstractController
 
     /**
      * @Route("/auto", name="messages:auto")
+     * @param Request $request
      * @param DocumentManager $dm
      * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function automaticMessages(DocumentManager $dm, PaginatorInterface $paginator): Response
+    public function automaticMessages(Request $request, DocumentManager $dm, PaginatorInterface $paginator): Response
     {
         return $this->render('messages/list.html.twig', [
             'title' => 'Messages rapides',
-            'messages' => $paginator->paginate($dm->getRepository(Message::class)->findBy(['messageType' => 'auto']))
+            'messages' => $paginator->paginate(
+                $dm->getRepository(Message::class)->findBy(['messageType' => 'auto']),
+                $request->query->getInt('page', 1)
+            )
         ]);
     }
 
     /**
      * @Route("/addonpack", name="messages:addonpack")
+     * @param Request $request
      * @param DocumentManager $dm
      * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function addonPacks(DocumentManager $dm, PaginatorInterface $paginator): Response
+    public function addonPacks(Request $request, DocumentManager $dm, PaginatorInterface $paginator): Response
     {
         return $this->render('messages/list.html.twig', [
             'title' => 'Packs d\'add-ons',
-            'messages' => $paginator->paginate($dm->getRepository(Message::class)->findBy(['messageType' => 'addonpack']))
+            'messages' => $paginator->paginate(
+                $dm->getRepository(Message::class)->findBy(['messageType' => 'addonpack']),
+                $request->query->getInt('page', 1)
+            )
         ]);
     }
 
     /**
      * @Route("/error", name="messages:error")
+     * @param Request $request
      * @param DocumentManager $dm
      * @param PaginatorInterface $paginator
      * @return Response
      */
-    public function errorDetails(DocumentManager $dm, PaginatorInterface $paginator): Response
+    public function errorDetails(Request $request, DocumentManager $dm, PaginatorInterface $paginator): Response
     {
         return $this->render('messages/list.html.twig', [
             'title' => 'Détails erreurs',
-            'messages' => $paginator->paginate($dm->getRepository(Message::class)->findBy(['messageType' => 'error']))
+            'messages' => $paginator->paginate(
+                $dm->getRepository(Message::class)->findBy(['messageType' => 'error']),
+                $request->query->getInt('page', 1)
+            )
         ]);
     }
 
@@ -141,6 +156,156 @@ class MessageController extends AbstractController
 
         $this->addFlash('success', 'Votre suggestion de modification a été enregistrée. Elle sera traitée prochainement !');
         return $this->redirectToRoute('messages');
+    }
+
+    /**
+     * @Route("/view/{messageId}", name="messages:view")
+     * @param Request $request
+     * @param DocumentManager $dm
+     * @return Response
+     */
+    public function viewEdit(Request $request, DocumentManager $dm): Response
+    {
+        $messageRequest = $dm->getRepository(MessageEditRequest::class)->findOneBy(['_id' => $request->get('messageId')]);
+        if (!$messageRequest) return new RedirectResponse($this->generateUrl('messages:waiting'));
+        return $this->render('messages/view.html.twig', [
+            'request' => $messageRequest
+        ]);
+    }
+
+    /**
+     * @Route("/waiting", name="messages:waiting")
+     * @IsGranted("ROLE_STAFF")
+     * @param DocumentManager $dm
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    public function waitingRequests(DocumentManager $dm, PaginatorInterface $paginator): Response
+    {
+        $messages = $paginator->paginate(
+            $dm->createQueryBuilder(MessageEditRequest::class)
+                ->field('validated')->equals(null)
+                ->getQuery()
+        );
+        return $this->render('messages/admin.html.twig', [
+            'title' => 'Messages en attente',
+            'requests' => $messages
+        ]);
+    }
+
+    /**
+     * @Route("/accepted", name="messages:accepted")
+     * @IsGranted("ROLE_STAFF")
+     * @param DocumentManager $dm
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    public function acceptedRequests(DocumentManager $dm, PaginatorInterface $paginator): Response
+    {
+        $messages = $paginator->paginate(
+            $dm->createQueryBuilder(MessageEditRequest::class)
+                ->field('validated')->equals(true)
+                ->getQuery()
+        );
+        return $this->render('messages/admin.html.twig', [
+            'title' => 'Messages acceptés',
+            'requests' => $messages
+        ]);
+    }
+
+    /**
+     * @Route("/denied", name="messages:denied")
+     * @IsGranted("ROLE_STAFF")
+     * @param DocumentManager $dm
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    public function deniedRequests(DocumentManager $dm, PaginatorInterface $paginator): Response
+    {
+        $messages = $paginator->paginate(
+            $dm->createQueryBuilder(MessageEditRequest::class)
+                ->field('validated')->equals(false)
+                ->getQuery()
+        );
+        return $this->render('messages/admin.html.twig', [
+            'title' => 'Messages refusés',
+            'requests' => $messages
+        ]);
+    }
+
+    /**
+     * @Route("/approve", name="messages:approve", methods={"POST"})
+     * @param Request $request
+     * @param DocumentManager $dm
+     * @return Response
+     * @throws MongoDBException
+     */
+    public function approveEdit(Request $request, DocumentManager $dm): Response
+    {
+        $messageId = $request->request->get('messageId');
+        $isValidated = $request->request->getBoolean('validated');
+        $message = $dm->getRepository(MessageEditRequest::class)->findOneBy(['_id' => $messageId]);
+        if (!$message || !isset($isValidated)) {
+            $this->addFlash('error', 'Votre requête est invalide.');
+            return $this->redirectToRoute('messages:view', ['messageId' => $messageId]);
+        }
+        if (!$this->isGranted('ROLE_STAFF')) {
+            $this->addFlash('error', 'Vous n\'avez pas la permission d\'approuver ce message.');
+            return $this->redirectToRoute('messages:view', ['messageId' => $messageId]);
+        }
+        /** @var Message $targetMessage */
+        $targetMessage = $message->getMessage();
+        /** @var User $reviewer */
+        $reviewer = $this->getUser();
+
+        if (!$targetMessage) { // It's a new message
+            $newMessage = new Message();
+            $newMessage->setName($message->getNewName());
+            $newMessage->setAliases($message->getNewAliases());
+            $newMessage->setContent($message->getNewContent());
+            $newMessage->setMessageType($message->getMessageType());
+            $dm->persist($newMessage);
+            $dm->flush();
+        } else if ($isValidated) {
+            $dm->createQueryBuilder(Message::class)
+                ->findAndUpdate()
+                ->field('_id')->equals($targetMessage->getId())
+                ->field('name')->set($message->getNewName())
+                ->field('aliases')->set($message->getNewAliases())
+                ->field('content')->set($message->getNewContent())
+                ->getQuery()
+                ->execute();
+        }
+        $dm->createQueryBuilder(MessageEditRequest::class)
+            ->findAndUpdate()
+            ->field('_id')->equals($message->getId())
+            ->field('validated')->set($isValidated)
+            ->field('reviewer')->set($reviewer)
+            ->getQuery()
+            ->execute();
+        $this->addFlash('success', 'Cette suggestion de modification a été marquée comme ' . ($isValidated ? 'validée' : 'refusée') . '.');
+        return $this->redirectToRoute('messages:waiting');
+    }
+
+    /**
+     * @Route("/{messageId}", name="messages:edit", methods={"GET"})
+     * @param Request $request
+     * @param DocumentManager $dm
+     * @return Response
+     */
+    public function editMessage(Request $request, DocumentManager $dm): Response
+    {
+        $message = $dm->getRepository(Message::class)->findOneBy(['_id' => $request->get('messageId')]);
+        if (!$message) return new RedirectResponse($this->generateUrl('messages'));
+        $messageEditRequest = $dm->createQueryBuilder(MessageEditRequest::class)
+            ->field('message.id')->equals($message->getId())
+            ->field('validated')->equals(null)
+            ->getQuery()
+            ->getSingleResult();
+        return $this->render('messages/edit.html.twig', [
+            'addonPack' => $message,
+            'editForbidden' => isset($messageEditRequest)
+        ]);
     }
 
 }
