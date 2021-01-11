accb9b1016797563d2b3906f55b927b8247247c7
Romitou <romitou@protonmail.com>
2021-01-11 21:11:50 +0100

    :chart_with_upwards_trend: Add Sentry to catch real-time errors

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
