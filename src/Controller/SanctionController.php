772503fe916db9fb100f1c32851337725f779ab7
Romitou <romitou@protonmail.com>
2021-01-24 14:12:26 +0100

    :sparkles: Use Symfony Forms with query parameters for more convenience (closes #52)

--- src/Controller/SanctionController.php
+++ src/Controller/SanctionController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Document\Moderation\Sanction;
+use App\Entity\SanctionQuery;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use Doctrine\ODM\MongoDB\MongoDBException;
 use Knp\Component\Pager\PaginatorInterface;
 use MongoDB\BSON\Regex;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,65 +33,72 @@ class SanctionController extends AbstractController
      */
     public function home(Request $request, DocumentManager $dm, PaginatorInterface $paginator): Response
     {
-        $sanctions = $paginator->paginate(
-            $dm->createQueryBuilder(Sanction::class)
-                ->find()
-                ->sort('_id', 'DESC')
-                ->getQuery(),
-            $request->query->getInt('page', 1)
-        );
+
+        $sanctionQuery = new SanctionQuery();
+        $form = $this->createFormBuilder($sanctionQuery)
+            ->setMethod('GET')
+            ->add('memberId', TextType::class, [
+                'required' => false
+            ])
+            ->add('moderatorId', TextType::class, [
+                'required' => false
+            ])
+            ->add('reason', TextType::class, [
+                'required' => false
+            ])
+            ->add('sanctionStatus', ChoiceType::class, [
+                'required' => false,
+                'choices' => [
+                    'Terminée' => true,
+                    'En cours' => false
+                ]
+            ])
+            ->add('beforeDate', DateTimeType::class, [
+                'required' => false,
+                'input' => 'timestamp',
+                'widget' => 'single_text'
+            ])
+            ->add('afterDate', DateTimeType::class, [
+                'required' => false,
+                'input' => 'timestamp',
+                'widget' => 'single_text'
+            ])
+            ->add('sanctionType', ChoiceType::class, [
+                'required' => false,
+                'choices' => [
+                    'Bannissement Discord' => 'hardban',
+                    'Bannissement' => 'ban',
+                    'Mute' => 'mute',
+                    'Avertissement' => 'warn'
+                ]
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Rechercher'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        $query = $dm->createQueryBuilder(Sanction::class)
+            ->find()
+            ->sort('_id', 'DESC');
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var SanctionQuery $data */
+            $data = $form->getData();
+            if ($data->getMemberId()) $query->field('memberId')->equals($data->getMemberId());
+            if ($data->getModeratorId()) $query->field('moderator')->equals($data->getModeratorId());
+            if ($data->getReason()) $query->field('reason')->equals(new Regex('/' . $data->getReason() . '/'));
+            if ($data->getSanctionStatus() !== null) $query->field('revoked')->equals($data->getSanctionStatus());
+            if ($data->getSanctionType()) $query->field('type')->equals($data->getSanctionType());
+            if ($data->getSanctionId()) $query->field('sanctionId')->equals($data->getSanctionId());
+            if ($data->getAfterDate()) $query->field('start')->gte($data->getAfterDate() * 1000);
+            if ($data->getBeforeDate()) $query->field('start')->lte($data->getBeforeDate() * 1000);
+        }
         return $this->render('sanctions/home.html.twig', [
-            'sanctions' => $sanctions
+            'sanctions' => $paginator->paginate(
+                $query->getQuery(),
+                $request->query->getInt('page', 1)
+            ),
+            'search' => $form->createView()
         ]);
-    }
-
-    /**
-     * @Route("/search", name="sanctions:search", methods={"POST"})
-     * @param Request $request
-     * @param DocumentManager $dm
-     * @return Response
-     */
-    public function search(Request $request, DocumentManager $dm): Response
-    {
-        $memberId = $request->request->get('memberId');
-        $moderatorId = $request->request->get('moderatorId');
-        $reason = $request->request->get('reason');
-        $sanctionStatus = $request->request->get('sanctionStatus');
-        $afterDate = $request->request->get('afterDate');
-        $beforeDate = $request->request->get('beforeDate');
-        $sanctionType = $request->request->get('sanctionType');
-        $query = $dm->createQueryBuilder(Sanction::class);
-        if (isset($memberId) && $memberId !== "")
-            $query->field('memberId')->equals($memberId);
-        if (isset($moderatorId) && $moderatorId !== "")
-            $query->field('moderator')->equals($moderatorId);
-        if (isset($reason) && $reason !== "")
-            $query->field('reason')->equals(new Regex('/' . $reason . '/'));
-        if (isset($sanctionStatus) && $sanctionStatus !== "")
-            $query->field('revoked')->equals($sanctionStatus == "true");
-        if (isset($afterDate) && $afterDate !== "") {
-            $date = strtotime($afterDate);
-            $query->field('start')->gte($date * 1000);
-        }
-        if (isset($beforeDate) && $beforeDate !== "") {
-            $date = strtotime($beforeDate);
-            $query->field('start')->lte($date * 1000);
-        }
-        if (isset($sanctionType) && $sanctionType !== "")
-            $query->field('type')->equals($sanctionType);
-        try {
-            $results = $query
-                ->sort('start', 'DESC')
-                ->getQuery()
-                ->execute();
-        } catch (MongoDBException $e) {
-            return $this->redirectToRoute('messages');
-        }
-
-        return $this->render('sanctions/search.html.twig', [
-            'results' => $results
-        ]);
-
     }
 
 }
---
+++ src/Entity/SanctionQuery.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+class SanctionQuery
+{
+
+    protected ?string $memberId = null;
+
+    protected ?string $sanctionStatus = null;
+
+    protected ?string $moderatorId = null;
+
+    protected ?int $afterDate = null;
+
+    protected ?int $beforeDate = null;
+
+    protected ?string $reason = null;
+
+    protected ?string $sanctionType = null;
+
+    protected ?string $sanctionId = null;
+
+    /**
+     * @return string|null
+     */
+    public function getMemberId(): ?string
+    {
+        return $this->memberId;
+    }
+
+    /**
+     * @param string|null $memberId
+     */
+    public function setMemberId(?string $memberId): void
+    {
+        $this->memberId = $memberId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSanctionStatus(): ?string
+    {
+        return $this->sanctionStatus;
+    }
+
+    /**
+     * @param string|null $sanctionStatus
+     */
+    public function setSanctionStatus(?string $sanctionStatus): void
+    {
+        $this->sanctionStatus = $sanctionStatus;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getModeratorId(): ?string
+    {
+        return $this->moderatorId;
+    }
+
+    /**
+     * @param string|null $moderatorId
+     */
+    public function setModeratorId(?string $moderatorId): void
+    {
+        $this->moderatorId = $moderatorId;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getAfterDate(): ?int
+    {
+        return $this->afterDate;
+    }
+
+    /**
+     * @param int|null $afterDate
+     */
+    public function setAfterDate(?int $afterDate): void
+    {
+        $this->afterDate = $afterDate;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getBeforeDate(): ?int
+    {
+        return $this->beforeDate;
+    }
+
+    /**
+     * @param int|null $beforeDate
+     */
+    public function setBeforeDate(?int $beforeDate): void
+    {
+        $this->beforeDate = $beforeDate;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReason(): ?string
+    {
+        return $this->reason;
+    }
+
+    /**
+     * @param string|null $reason
+     */
+    public function setReason(?string $reason): void
+    {
+        $this->reason = $reason;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSanctionType(): ?string
+    {
+        return $this->sanctionType;
+    }
+
+    /**
+     * @param string|null $sanctionType
+     */
+    public function setSanctionType(?string $sanctionType): void
+    {
+        $this->sanctionType = $sanctionType;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSanctionId(): ?string
+    {
+        return $this->sanctionId;
+    }
+
+    /**
+     * @param string|null $sanctionId
+     */
+    public function setSanctionId(?string $sanctionId): void
+    {
+        $this->sanctionId = $sanctionId;
+    }
+
+}
