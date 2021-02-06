6ceb903763cf09eaa41cfeac6a951056cafa8213
Romitou <romitou@protonmail.com>
2021-02-06 14:28:10 +0100

    :sparkles: Add user edition

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Document\User;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\MongoDBException;
 use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -51,11 +53,41 @@ class UserController extends AbstractController
      * @Route("/{userId}", name="users:edit", methods={"POST"})
      * @param int $userId
      * @param Request $request
+     * @param DocumentManager $dm
      * @return Response
+     * @throws MongoDBException
      */
-    public function editUser(int $userId, Request $request): Response
+    public function editUser(int $userId, Request $request, DocumentManager $dm): Response
     {
-        // TODO
+        $username = $request->request->get('discordUsername');
+        $avatarUrl = $request->request->get('discordAvatar');
+        $dashboardRole = $request->request->get('dashboardRole');
+        $targetUser = $dm->getRepository(User::class)->findOneBy(['_id' => $userId]);
+        if (!$username || !$avatarUrl || !$dashboardRole || !$targetUser || !in_array($dashboardRole, ['ROLE_USER', 'ROLE_STAFF', 'ROLE_ADMIN'])) {
+            $this->addFlash('error', 'Certains champs sont vides ou incorrects.');
+            return new RedirectResponse($this->generateUrl('users:view', ['userId' => $userId]));
+        }
+
+        if (!$this->isGranted($dashboardRole)) {
+            $this->addFlash('error', 'Vous n\'avez pas la permission de modifier cet utilisateur.');
+            return new RedirectResponse($this->generateUrl('users:view', ['userId' => $userId]));
+        }
+
+        $targetUser->setUsername($username);
+        $targetUser->setAvatarUrl($avatarUrl);
+        $targetUser->setDiscordRoles([$dashboardRole]);
+
+        $dm->createQueryBuilder(User::class)
+            ->updateOne()
+            ->field('_id')->equals($userId)
+            ->field('username')->set($username)
+            ->field('avatarUrl')->set($avatarUrl)
+            ->field('roles')->set([$dashboardRole])
+            ->getQuery()
+            ->execute();
+
+        $this->addFlash('success', 'Vos modifications ont bien été enregistrées.');
+        return new RedirectResponse($this->generateUrl('users:view', ['userId' => $userId]));
     }
 
 }
